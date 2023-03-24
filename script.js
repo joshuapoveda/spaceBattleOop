b52ec4e23850cc1e552f7e6e44a33b9097208a36
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-24 16:27:26 -0400

    Functional game. Comments = extra features in-progress

--- script.js
+++ script.js
@@ -4,21 +4,58 @@
 
 // There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
 
-//<button class="attack">Start the first volley!</button>;
-
-////MAIN CHARACTER OBJECT
-
 function graphics() {
+  ////IMPLEMENT STATIC CLASS TO HANDLE HEAVY LIFTING OF GRAPHICS CREATION
+
+  document.body.style.backgroundColor = "rgb(36, 49, 74)";
+
   const title = document.createElement("h1");
   title.textContent = "Space!";
   title.style.color = "rgb(177,161,139)";
   document.body.append(title);
   document.body.style.backgroundColor = "rgb(36, 49, 74)";
+
+  // const imgBoxContainer = document.createElement("div");
+  // imgBoxContainer.style.backgroundColor = "black";
+  // imgBoxContainer.style.border = "solid 3px black";
+  // imgBoxContainer.style.height = "130px";
+  // imgBoxContainer.style.width = "100%";
+  // imgBoxContainer.style.position = "relative";
+  // imgBoxContainer.style.display = "flex";
+  // //imgBoxContainer.style.margin = "auto";
+
+  // const imgBox2 = document.createElement("div");
+  // imgBox2.style.backgroundColor = "red";
+  // imgBox2.style.border = "solid 3px black";
+  // imgBox2.style.height = "100px";
+  // imgBox2.style.width = "100px";
+  // imgBox2.style.position = "relative";
+  // imgBox2.style.justifyContent = "flex-start";
+
+  // const imgBox3 = document.createElement("div");
+  // imgBox3.style.backgroundColor = "yellow";
+  // imgBox3.style.border = "solid 3px black";
+  // imgBox3.style.height = "100px";
+  // imgBox3.style.width = "100px";
+  // imgBox3.style.position = "relative";
+  // imgBox3.style.justifyContent = "flex-start";
+
+  // document.body.append(imgBoxContainer);
+  // document.querySelector("div").append(imgBox2);
+  // document.querySelector("div").append(imgBox3);
+
+  /////////////LINE BREAKS////////////////
+  // linebreak = document.createElement("br");
+  // document.body.appendChild(linebreak);
+  // linebreak = document.createElement("br");
+  // document.body.appendChild(linebreak);
+  /////////////LINE BREAKS////////////////
 }
 graphics();
 
+////MAIN CHARACTER OBJECT
 class Player {
-  //////ADDED FUCNTIONALITY, CHARACTER CREATION
+  //////ADDED FUCNTIONALITY, CHARACTER CREATION:
   //let gameIntro = window.prompt("test");
   // if (gameIntro.toLowerCase() === "test") {
   //   console.log('testPassed');
@@ -41,11 +78,13 @@ class Player {
 }
 
 ////ALIEN CHARACTER OBJECT.
+//NOTE for future implementation: If you pass the Alien object as a property, then it owns its own html element and can manipulate it accordingly via methods
 class Alien {
-  constructor(hull, firepower, accuracy) {
+  constructor(hull, firepower, accuracy, htmlElem) {
     this.hull = hull;
     this.firepower = firepower;
     this.accuracy = accuracy;
+    this.htmlElement = this.htmlElement;
   }
   //RETURNS PLAYER'S HULL HEALTH AFTER ATTACK (INCLUDING HIT OR MISS PROBABILITY)
   enemyAttack() {
@@ -56,7 +95,9 @@ class Alien {
         variableText.style.color = "green";
       } else if (funcText === "We have been hit, sir.") {
         variableText.style.color = "red";
-        variableText.textContent = `${funcText} ----> Hull Status ${ussAssembly.hull - this.firepower} HP.`;
+        variableText.textContent = `${funcText} ----> Hull Status ${
+          ussAssembly.hull - this.firepower
+        } HP.`;
       } else {
         variableText.style.color = "orange";
       }
@@ -67,7 +108,7 @@ class Alien {
     textGenerator("Incoming attack!");
 
     if (Math.random() < Aliens[0].accuracy) {
-      console.log('%c "We have been hit, sir."',"font-size: 20px; color: red");
+      console.log('%c "We have been hit, sir."', "font-size: 20px; color: red");
       textGenerator("We have been hit, sir.");
       return ussAssembly.hull - this.firepower;
     } else {
@@ -79,7 +120,6 @@ class Alien {
 }
 
 ////WILL GENERATE ALL OBJECTS.
-
 class Factory {
   generatePlayer() {
     const mainPlayer = new Player(20, 5, 0.7);
@@ -103,7 +143,7 @@ class Factory {
     return arrayOfAliens;
   }
 }
-////NEW SET OF ALIENS
+//NEW SET OF ALIENS
 const setAliens = (num) => {
   const setOfAliens = new Factory();
   return setOfAliens.generateAliens(num);
@@ -112,7 +152,7 @@ const setAliens = (num) => {
 function getRandomNum2(min, max) {
   return Math.random() * (max - min) + min;
 }
-let randNumGen = Math.floor(getRandomNum2(7, 20))
+let randNumGen = Math.floor(getRandomNum2(7, 20));
 const Aliens = setAliens(randNumGen);
 
 ////NEW PLAYER
@@ -139,11 +179,12 @@ const roundSimulation = () => {
       variableText.textContent = `${funcText}`;
       variableText.style.color = "white";
     }
+
     document.body.append(variableText);
   };
 
   if (attackOutcome <= 0) {
-    console.log('%c "Lasers fired!"',"font-size: 20px; color: beige");
+    console.log('%c "Lasers fired!"', "font-size: 20px; color: beige");
     textGenerator2("Lasers fired!");
     Aliens.shift();
 
@@ -152,13 +193,16 @@ const roundSimulation = () => {
 
     console.table(Aliens);
   } else {
-    console.log('%c "Lasers fired! Enemy damaged."',"font-size: 20px; color: yellow");
+    console.log(
+      '%c "Lasers fired! Enemy damaged."',
+      "font-size: 20px; color: yellow"
+    );
     textGenerator2("Lasers fired! Enemy damaged.");
 
     let attackOutcome2 = Aliens[0].enemyAttack();
     Aliens[0].hull = attackOutcome;
     ussAssembly.hull = attackOutcome2;
-    console.table(Aliens[0])
+    console.table(Aliens[0]);
     console.table(ussAssembly);
   }
 
@@ -183,11 +227,19 @@ function btns() {
   document.body.appendChild(fire);
   fire.classList.add("attack");
   fire.addEventListener("click", roundSimulation);
-
+////THIS FEATURE REMOVES PREVIOUS  APPENDED H3s. KEEPING OUTPUT IN ONE AREA OF BROWSER
+  // fire.addEventListener("mousedown", function () {
+  //   firstTextPrint = document.querySelectorAll("h3");
+  //   for (elem of firstTextPrint) {
+  //     elem.remove();
+  //   }
+  // });
+  /////////////LINE BREAKS////////////////
   linebreak = document.createElement("br");
   document.body.appendChild(linebreak);
   linebreak = document.createElement("br");
   document.body.appendChild(linebreak);
+  /////////////LINE BREAKS////////////////
 
   const retreat = document.createElement("BUTTON");
   let text2 = document.createTextNode("Retreat");
